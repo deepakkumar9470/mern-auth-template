a77f501cbc8261c24ac4623989f50dfe3e02141f
deepakkumar9470 <dk397787@gmail.com>
2022-04-27 09:46:32 +0530

    mern-auth-template

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router()
+
+import UserController from '../controllers/userController.js'
+
+import checkUserAuth from '../middleware/auth-middleware.js'
+
+
+// Protected Routes
+router.use('/changepassword', checkUserAuth)
+
+
+// Register Route
+// @ /api/user/register
+
+router.post('/register', UserController.userregister)
+
+
+// Login Route
+// @ /api/user/login
+router.post('/login', UserController.userlogin)
+
+
+// Change Password Route
+// @ /api/user/changepassword
+router.post('/changepassword', UserController.changepassword)
+
+
+
+export default router
